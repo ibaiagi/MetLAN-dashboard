c644668f5631415942f0c55ac47b0171da3f692c
Ibai Agirresarobe <agirresarobeibai@gmail.com>
2026-09-26 00:39:58 +0200

    feat(modem): add enable()/disable() dial control

--- app/modem_service.py
+++ app/modem_service.py
@@ -13,12 +13,30 @@ STATUS_DICT = {
     },
 }
 
-def _check_connection_status() -> str:
+def _init() -> tuple[Connection, Client]:
     conn = Connection(URL)
-    status = Client(connection=conn).monitoring.status()
+    status = Client(connection=conn)
+    return conn, status
+
+def check_connection_status(client: Client) -> str:
+    status = client.monitoring.status()
     if not status["ConnectionStatus"] in STATUS_DICT["ConnectionStatus"]:
         return "Unknown"
     return STATUS_DICT["ConnectionStatus"][status["ConnectionStatus"]]
 
+def enable(client: Client):
+    client.dial_up.set_mobile_dataswitch(dataswitch=1)
+    client.dial_up.dial()
+
+def disable(client: Client):
+    client.dial_up._session.post_set(
+        "dialup/dial",
+        {
+            "Action": 0,
+        },
+    )
+
 if __name__ == "__main__":
-    print(f" status: {_check_connection_status()}")
+    conn, client = _init()
+    print(f" status: {check_connection_status(client)}")
+    enable(client)
